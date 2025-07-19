a2c414bbc05675dd1719e129fb04560a62010c59
sprunk <spr.ng@o2.pl>
2025-07-19 02:08:38 +0200

    Version 4.911

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ return {
 		shortGame = "KP",
 		name = "Kernel Panic",
 		shortName = "KP",
-		version = "4.91",
+		version = "4.911",
 		mutator = "Fixes for 105",
 		description = "Digital Warfare!",
 		url = "http://springrts.com/wiki/Kernel_Panic",
